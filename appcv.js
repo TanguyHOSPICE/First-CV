1a72d9bde040d7dd419f375df8c1bfbfa3a4f7b8
hospice tanguy <htcvhs971@gmail.com>
2022-10-10 19:51:53 +0200

    Premier CV

---
+++ appcv.js
@@ -0,0 +1,208 @@
+var controller = new ScrollMagic.Controller();
+
+var scene = new ScrollMagic.Scene({
+	triggerElement: ".b1",
+	reverse: true,
+})
+
+	.setClassToggle(".b1", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene2 = new ScrollMagic.Scene({
+	triggerElement: ".b2",
+	reverse: true,
+})
+
+	.setClassToggle(".b2", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene3 = new ScrollMagic.Scene({
+	triggerElement: ".b3",
+	reverse: true,
+})
+
+	.setClassToggle(".b3", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene4 = new ScrollMagic.Scene({
+	triggerElement: ".b4",
+	reverse: true,
+})
+
+	.setClassToggle(".b4", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene5 = new ScrollMagic.Scene({
+	triggerElement: ".b5",
+	reverse: true,
+})
+
+	.setClassToggle(".b5", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene6 = new ScrollMagic.Scene({
+	triggerElement: ".b6",
+	reverse: true,
+})
+
+	.setClassToggle(".b6", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene7 = new ScrollMagic.Scene({
+	triggerElement: ".b7",
+	reverse: true,
+})
+
+	.setClassToggle(".b7", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene8 = new ScrollMagic.Scene({
+	triggerElement: ".b8",
+	reverse: true,
+})
+
+	.setClassToggle(".b8", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+var scene = new ScrollMagic.Scene({
+	triggerElement: ".r1",
+	reverse: true,
+})
+
+	.setClassToggle(".r1", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene2 = new ScrollMagic.Scene({
+	triggerElement: ".r2",
+	reverse: true,
+})
+
+	.setClassToggle(".r2", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene3 = new ScrollMagic.Scene({
+	triggerElement: ".r3",
+	reverse: true,
+})
+
+	.setClassToggle(".r3", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene4 = new ScrollMagic.Scene({
+	triggerElement: ".r4",
+	reverse: true,
+})
+
+	.setClassToggle(".r4", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene5 = new ScrollMagic.Scene({
+	triggerElement: ".r5",
+	reverse: true,
+})
+
+	.setClassToggle(".r5", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene6 = new ScrollMagic.Scene({
+	triggerElement: ".r6",
+	reverse: true,
+})
+
+	.setClassToggle(".r6", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene7 = new ScrollMagic.Scene({
+	triggerElement: ".r7",
+	reverse: true,
+})
+
+	.setClassToggle(".r7", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
+
+var scene8 = new ScrollMagic.Scene({
+	triggerElement: ".r8",
+	reverse: true,
+})
+
+	.setClassToggle(".r8", "fade-in")
+	.addIndicators({
+		name: "INDICATIONS",
+		indent: 200,
+		colorStart: "#fff",
+	})
+	.addTo(controller);
